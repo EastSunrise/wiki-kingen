e6c35cb72e041d6f39917b2f354329ce107f754c
kingen <wsg787@126.com>
2024-01-05 15:45:57 +0800

    Deployed 6483db3 with MkDocs version: 1.4.3

--- static/js/kingen.js
+++ static/js/kingen.js
@@ -10,5 +10,5 @@ for (let a of document.getElementsByTagName("a")) {
 for (let footer of document.getElementsByTagName("footer")) {
     footer.innerHTML =
         "<p style='text-align:center;'><a href='https://github.com/eastsunrise/wiki-kingen'>wiki-kingen - GitHub</a>&emsp;" +
-        "|&emsp;<a href='https://eastsunrise.github.io/wiki-kingen-en/'>英文版</a></p>";
+        "|&emsp;<a href='/wiki-kingen-en/'>英文版</a></p>";
 }
